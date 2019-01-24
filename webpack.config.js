9090abc1e90fba992d2773402695f7ea9a358414
Pawel Butra <pawel.butra@onet.pl>
2019-01-24 19:30:43 +0100

    WebPack config Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const path = require('path');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'app.bundle.js'
-	},
-	module: {
-		rules: [
-		    {
-		        test: /\.js$/,
-		        loader: "babel-loader"
-		    },
-		    {
-		        test: /\.css$/,
-		        use: [
-		            { loader: 'style-loader'},
-		            {
-		                loader: 'css-loader',
-		                options: {
-		                    modules: true
-		                }
-		            }
-		        ]
-		    }
-		]
+module.exports =  (env) => {
+		const enviroment = env || 'production';
+
+		return {
+		mode: enviroment,
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'app.' + enviroment + '.bundle.js'
+		},
+		optimization: {
+			minimize: false
+		},
+		module: {
+			rules: [
+			    {
+			        test: /\.js$/,
+			        loader: "babel-loader"
+			    },
+			    {
+			        test: /\.css$/,
+			        use: [
+			            { loader: 'style-loader'},
+			            {
+			                loader: 'css-loader',
+			                options: {
+			                    modules: true
+			                }
+			            }
+			        ]
+			    }
+			]
+		}
 	}
 };
